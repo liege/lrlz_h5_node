2f0663752e253d18570c7b1d788b365a0a8b315c
vincent <vincentwangjiling@gmail.com>
2014-11-13 22:23:20 +0800

    testdrirver.js

---
+++ driver/testdriver.js
@@ -0,0 +1,13 @@
+var Driver = require('./index.js');
+
+Driver.queryDataByGet('http://112.64.126.126:49521/TAEServer/app/inter/getProductInfo.ss', {uuid: 'IJ45014z98'},function(returnData){
+    console.log('returnGetData: ' + JSON.stringify(returnData));
+});
+
+Driver.queryDataByPost('http://112.64.126.126:49521/TAEServer/app/inter/getProductInfo.ss', {uuid: 'IJ45014z98'},function(returnData){
+    console.log('returnPostData: ' + JSON.stringify(returnData));
+});
+
+Driver.post('http://112.64.126.126:49521/TAEServer/app/inter/getProductInfo.ss',{uuid: 'IJ45014z98'},function(data){
+    console.log('post: ' + JSON.stringify(data));
+});
