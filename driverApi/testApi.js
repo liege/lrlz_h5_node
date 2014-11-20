81525c058253742417f3c35ffa129ce9005a723b
vincent <vincentwangjiling@gmail.com>
2014-11-20 19:34:00 +0800

    modify testApi.js with adding query params

--- driverApi/testApi.js
+++ driverApi/testApi.js
@@ -1,17 +1,17 @@
 var DriverApi = require('./index.js');
 
-DriverApi.getProductInfo({uuid: 'IJ45014z98'},function(returnData){
+DriverApi.getProductInfo({uuid: 'IJ45014z98', appKey:'6581235709', appVer:'1.0'},function(returnData){
     console.log('getProductInfo: ' + JSON.stringify(returnData));
 });
 
-DriverApi.getRecommandList({uuid: 'IJ45014z98'},function(returnData){
+DriverApi.getRecommandList({uuid: 'IJ45014z98', appKey:'6581235709', appVer:'1.0'},function(returnData){
     console.log('getRecommandList: ' + JSON.stringify(returnData));
 });
 
-DriverApi.getCommentList({uuid: 'IJ45014z98', limit: '10', type: '1'},function(returnData){
+DriverApi.getCommentList({uuid: 'IJ45014z98', limit: '10', type: '1', appKey:'6581235709', appVer:'1.0'},function(returnData){
     console.log('getCommentList: ' + JSON.stringify(returnData));
 });
 
-DriverApi.getDetailData({uuid: 'IJ45014z98',last_modified: '0'},function(returnData){
+DriverApi.getDetailData({uuid: 'IJ45014z98',last_modified: '0', appKey:'6581235709', appVer:'1.0'},function(returnData){
     console.log('getDetail: ' + JSON.stringify(returnData));
 });
